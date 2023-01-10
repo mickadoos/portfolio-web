7dec8c87363069562ae8a306d5ac0ed868f23efe
Yabel <yabelrr@gmail.com>
2023-01-10 11:02:46 +0100

    Firs commit, app css implemented, working on diferent sections

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+
+const Nav = () => {
+  return (
+    <div>
+    <nav>
+      <a href='# '><AiOutlineHome/></a>
+      <a href='#about'><AiOutlineUser/></a>
+      <a href='#experience'><BiBook/></a>
+      <a href='#services'><RiServiceLine/></a>
+      <a href='#contact'><BiMessageSquareDetail/></a>
+    </nav>
+    </div>
+  )
+}
+
+export default Nav
