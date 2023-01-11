0ddffdd65c83fcb399ce438775e2dbc58a5443cf
Yabel <yabelrr@gmail.com>
2023-01-11 16:12:15 +0100

    We are done wi the web. So lets deploy it in the internet

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,98 @@
-import React from 'react'
-import './Portfolio.css'
+import React from "react";
+import "./Portfolio.css";
+import IMG1 from "../../assets/aso-friends.jpg";
+import IMG2 from "../../assets/belfast-zoo.jpg";
+import IMG3 from "../../assets/bowling.jpg";
+import IMG4 from "../../assets/escalada-pro.jpg";
+import IMG5 from "../../assets/DPlan.png";
+import IMG6 from "../../assets/friki-festa.jpg";
 
 const Portfolio = () => {
-  return (
-    <section id='portfolio'>Portfolio</section>
-  )
-}
 
-export default Portfolio
+  const data = [
+    {
+      id: 1,
+      image: IMG1,
+      title: 'This is a portfolio item title',
+      github: 'https://github.com/mickadoos',
+      demo: 'https://github.com/mickadoos'
+    },
+    {
+      id: 2,
+      image: IMG2,
+      title: 'This is a portfolio item title',
+      github: 'https://github.com/mickadoos',
+      demo: 'https://github.com/mickadoos'
+    },
+    {
+      id: 3,
+      image: IMG3,
+      title: 'This is a portfolio item title',
+      github: 'https://github.com/mickadoos',
+      demo: 'https://github.com/mickadoos'
+    },
+    {
+      id: 4,
+      image: IMG4,
+      title: 'This is a portfolio item title',
+      github: 'https://github.com/mickadoos',
+      demo: 'https://github.com/mickadoos'
+    },
+    {
+      id: 5,
+      image: IMG5,
+      title: 'This is a portfolio item title',
+      github: 'https://github.com/mickadoos',
+      demo: 'https://github.com/mickadoos'
+    },
+    {
+      id: 6,
+      image: IMG6,
+      title: 'This is a portfolio item title',
+      github: 'https://github.com/mickadoos',
+      demo: 'https://github.com/mickadoos'
+    }
+  ]
+
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <container className="portfolio__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={image} alt="" />
+          </div>
+          <h3>{title}</h3>
+          <div className="portfolio__item-cta">
+          <a
+            href={github}
+            className="btn"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          <a
+            href={demo}
+            className="btn btn-primary"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Live Demo
+          </a>
+          </div>
+        </article>
+            )
+          })
+        }
+      </container>
+    </section>
+  );
+};
+
+export default Portfolio;
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,75 @@
-import React from 'react'
-import './Testimonials.css'
+import React from "react";
+import "./Testimonials.css";
+import AVTR1 from "../../assets/Yabel.png";
+import AVTR2 from "../../assets/Turró.png";
+import AVTR3 from "../../assets/JusiBP.png";
+import AVTR4 from "../../assets/Leticia.png";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper";
 
 const Testimonials = () => {
+  const data = [
+    {
+      avatar: AVTR1,
+      name: "Yabel Rodriguez",
+      review:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, illo? Voluptatibus labore ut soluta sunt?",
+    },
+    {
+      avatar: AVTR2,
+      name: "Josep Turro",
+      review:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, illo? Voluptatibus labore ut soluta sunt?",
+    },
+    {
+      avatar: AVTR3,
+      name: "Josep Bardes",
+      review:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, illo? Voluptatibus labore ut soluta sunt?",
+    },
+    {
+      avatar: AVTR4,
+      name: "Leticia Santos",
+      review:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, illo? Voluptatibus labore ut soluta sunt?",
+    },
+  ];
   return (
-    <section id='testimonials'>Testimonials</section>
-  )
-}
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
 
-export default Testimonials
+      <Swiper className="container testimonials__container"
+      //install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{clickable: true}}
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide  key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt="Avatar" />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">
+                {review}
+              </small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Testimonials;
